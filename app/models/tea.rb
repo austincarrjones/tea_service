fa49f653b042527302f3a6777ce852355a005520
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-08-15 22:36:18 -0600

    passing model tests

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -2,5 +2,5 @@ class Tea < ApplicationRecord
   has_many :subscriptions
 
   validates :title, presence: true
-  validates :price, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
